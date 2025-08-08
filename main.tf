f1702b632d7c787b67ae24fb7214bbfdc8bfb544
mikemonroe80015 <mikemonroe80015@hotmail.com>
2025-08-08 16:30:16 -0600

    autoscaling

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
+  version = "6.5.2"
   # insert the 1 required variable here
 
   name     = "blog"
@@ -44,7 +44,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = [module.blog_alb.target_group_arns]
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   
   image_id      = data.aws_ami.app_ami.id
